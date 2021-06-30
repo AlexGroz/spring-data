09339ef6ec0e4f9cbc803f1f54885bcbe6d0db0b
Alexey <groznovaa@gmail.comls>
2021-06-30 14:42:00 +0500

    Ovveride get, set. toString in the appliction class

--- src/main/java/com/javastart/springdata/entity/Account.java
+++ src/main/java/com/javastart/springdata/entity/Account.java
@@ -17,4 +17,57 @@ public class Account {
     private String email;
 
     private Integer bill;
+
+    public Account(Long id, String name, String email, Integer bill) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.bill = bill;
+    }
+
+    public  Account(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getBill() {
+        return bill;
+    }
+
+    public void setBill(Integer bill) {
+        this.bill = bill;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", bill=" + bill +
+                '}';
+    }
 }
